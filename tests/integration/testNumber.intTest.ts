3a086c35106379407cd5c6e3f34c2a3e528ed936
Adrian - Mihai Petrescu <adpetrescu@deloittece.com>
2019-03-19 17:45:32 +0200

    Remove the post test-number endpoint and update integration tests

--- tests/integration/testNumber.intTest.ts
+++ tests/integration/testNumber.intTest.ts
@@ -3,35 +3,29 @@ import {expect} from "chai";
 import {Injector} from "../../src/models/injector/Injector";
 import {TestNumberService} from "../../src/services/TestNumberService";
 import {TestNumber} from "../../src/models/TestNumber";
-import {Configuration} from "../../src/utils/Configuration";
-import supertest from "supertest";
-import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
+import {generateTestNumber} from "../../src/functions/generateTestNumber"
 
-const config: any = Configuration.getInstance().getConfig();
-const request = supertest(`http://localhost:${config.serverless.port}`);
+const LambdaTester = require('lambda-tester');
 const testNumberService: TestNumberService = Injector.resolve<TestNumberService>(TestNumberService);
-
-const postedActivity: DocumentClient.Key = {};
 
 describe("POST /test-number", () => {
 
     context("when a new test-number is requested the very first time(no data in db)", () => {
-            it("should respond with HTTP 200 and testNumber W01A001", () => {
-                const expectedFirstTestNumber: TestNumber = {
-                    testNumber: "W01A00108",
-                    id: "W01",
-                    certLetter: "A",
-                    sequenceNumber: "001"
-                };
-                return request.post("/test-number")
-                .send()
-                .expect("access-control-allow-origin", "*")
-                .expect("access-control-allow-credentials", "true")
-                .expect(200)
-                .then((response) => {
-                    expect(response.body).to.eql(expectedFirstTestNumber);
-                });
-            });
+        it("should respond with HTTP 200 and testNumber W01A001", () => {
+            const expectedFirstTestNumber: TestNumber = {
+                testNumber: "W01A00108",
+                id: "W01",
+                certLetter: "A",
+                sequenceNumber: "001"
+            };
+            return LambdaTester(generateTestNumber)
+            .expectResolve((response: any) => {
+                expect("access-control-allow-origin", "*")
+                expect("access-control-allow-credentials", "true")
+                expect(200)
+                expect(JSON.parse(response.body)).to.eql(expectedFirstTestNumber);
+            })
+        });
     });
 
     context("when a new test-number is requested when other test-numbers already exists in db", () => {
@@ -42,14 +36,13 @@ describe("POST /test-number", () => {
                 certLetter: "A",
                 sequenceNumber: "002"
             };
-            return request.post("/test-number")
-                .send()
-                .expect("access-control-allow-origin", "*")
-                .expect("access-control-allow-credentials", "true")
-                .expect(200)
-                .then((response) => {
-                    expect(response.body).to.eql(expectedFirstTestNumber);
-                });
+            return LambdaTester(generateTestNumber)
+            .expectResolve((response: any) => {
+                expect("access-control-allow-origin", "*")
+                expect("access-control-allow-credentials", "true")
+                expect(200)
+                expect(JSON.parse(response.body)).to.eql(expectedFirstTestNumber);
+            })
         });
     });
 
