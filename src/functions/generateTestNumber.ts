19dd8ccfd7fa800daa74bea145ac2fb43a6390a4
jonlazarini <jlazarini@deloitte.co.uk>
2021-07-13 11:21:13 +0100

    chore(cvsb-19465): add commitlint
    chore(cvsb-19465): add prettier for code formatting
    
    chore(cvsb-19465): add automated integration tests
    
    chore(cvsb-19465): update README and add hook
    
    chore(cvsb-19465): fix severless
    
    docs(cvsb-19465): update readme
    
    chore(cvsb-19465): fix comments

--- src/functions/generateTestNumber.ts
+++ src/functions/generateTestNumber.ts
@@ -1,19 +1,23 @@
-import {APIGatewayProxyResult, Context, Handler} from "aws-lambda";
-import {NumberService} from "../services/NumberService";
-import {HTTPResponse} from "../utils/HTTPResponse";
-import {TestNumber} from "../models/NumberModel";
-import {DynamoDBService} from "../services/DynamoDBService";
+import { APIGatewayProxyResult, Context, Handler } from "aws-lambda";
+import { NumberService } from "../services/NumberService";
+import { HTTPResponse } from "../utils/HTTPResponse";
+import { TestNumber } from "../models/NumberModel";
+import { DynamoDBService } from "../services/DynamoDBService";
 
-const generateTestNumber: Handler = async (event: any, context?: Context): Promise<APIGatewayProxyResult> => {
-    const numberService = new NumberService(new DynamoDBService());
-    return numberService.createTestNumber(1, null)
-        .then((testNumber: TestNumber) => {
-            return new HTTPResponse(200, testNumber);
-        })
-        .catch((error: HTTPResponse) => {
-            console.log(error.body);
-            return error;
-        });
+const generateTestNumber: Handler = async (
+  event: any,
+  context?: Context
+): Promise<APIGatewayProxyResult> => {
+  const numberService = new NumberService(new DynamoDBService());
+  return numberService
+    .createTestNumber(1, null)
+    .then((testNumber: TestNumber) => {
+      return new HTTPResponse(200, testNumber);
+    })
+    .catch((error: HTTPResponse) => {
+      console.log(error.body);
+      return error;
+    });
 };
 
-export {generateTestNumber};
+export { generateTestNumber };
