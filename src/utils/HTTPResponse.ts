1355b936086d526daaaeb9ca8012a4de9fab58fb
Daniel Searle <Daniel.searle1@DVSA.GOV.UKDaniel.searle1@DVSA.GOV.UKdaniel.searle1@dvsa.gov.uk>
2021-10-15 14:08:12 +0100

    feat(cvsb-19783): revert http response

--- src/utils/HTTPResponse.ts
+++ src/utils/HTTPResponse.ts
@@ -1,8 +1,8 @@
 import { APIGatewayProxyResult } from "aws-lambda";
 
-export class HTTPResponse implements APIGatewayProxyResult {
-  public body: any;
-  public statusCode: number;
+class HTTPResponse extends Error implements APIGatewayProxyResult {
+  public readonly statusCode: number;
+  public readonly body: any;
   public readonly headers: any;
 
   /**
@@ -12,15 +12,20 @@ export class HTTPResponse implements APIGatewayProxyResult {
    * @param headers - optional - the response headers
    */
   constructor(statusCode: number, body: any, headers = {}) {
+    super();
+
     if (headers) {
       this.headers = headers;
+      this.headers["Access-Control-Allow-Origin"] = "*";
+      this.headers["Access-Control-Allow-Credentials"] = true;
+      this.headers["X-Content-Type-Options"] = "nosniff";
+      this.headers.Vary = "Origin";
+      this.headers["X-XSS-Protection"] = "1; mode=block";
     }
-    this.headers["Access-Control-Allow-Origin"] = "*";
-    this.headers["Access-Control-Allow-Credentials"] = true;
-    this.headers["X-Content-Type-Options"] = "nosniff";
-    this.headers.Vary = "Origin";
-    this.headers["X-XSS-Protection"] = "1; mode=block";
+
     this.statusCode = statusCode;
     this.body = JSON.stringify(body);
   }
 }
+
+export { HTTPResponse };
